14893b9f994cf9d49977699c508cd25c861d897f
Daniel <gleek.dan@gmail.com>
2021-06-23 19:15:10 -0600

    Initial Setup

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import wave from '../images/login_signup_bottom_wave_placeholder.png'
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <img src={wave} alt="" />
+    </div>
+  );
+}
+
+export default App;
