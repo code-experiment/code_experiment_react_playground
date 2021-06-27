4d1acb7a940d468c86878821d0c62bd2a5503887
Daniel <gleek.dan@gmail.com>
2021-06-26 23:58:30 -0600

    Add HomePage, update routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import AuthPage from '../pages/AuthPage';
-import LoginPage from '../pages/LoginPage';
-import SignupPage from '../pages/SignupPage';
+import * as React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AuthPage from "../pages/AuthPage";
+import Home from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={AuthPage}/>
-        <Route path="/login" component={LoginPage}/>
-        <Route path="/sign-up" component={SignupPage}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/sign-up" component={SignupPage} />
       </Switch>
     </Router>
   );
