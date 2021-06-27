da49f4565e382d266d0a4a62fe4d140f0c764e02
Daniel <gleek.dan@gmail.com>
2021-06-27 14:41:26 -0600

    Add test for the Button component

---
+++ src/__tests__/components/Button.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Button from "../../components/Button";
+import userEvent from "@testing-library/user-event";
+
+test("Button renders correct text and onClick works correctly", () => {
+  let clicked = false;
+  render(<Button text="test" onClick={() => (clicked = true)} />);
+
+  const buttonElement = screen.getByRole("button", { name: "test" });
+  userEvent.click(buttonElement);
+
+  expect(clicked).toEqual(true);
+  expect(buttonElement).toBeInTheDocument();
+});
