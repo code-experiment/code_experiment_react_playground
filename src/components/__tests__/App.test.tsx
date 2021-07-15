f04f435b4af0cde182a0e69bbf25ae4fffc5efa4
Daniel <gleek.dan@gmail.com>
2021-07-14 19:47:05 -0600

    Moved test files

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from "../utils/test-utils";
-import App from '../components/App';
+import renderWithRouter from "../../utils/test-utils";
+import App from '../App';
 
 test('Make sure the Login button appears', () => {
   render(<App />);
