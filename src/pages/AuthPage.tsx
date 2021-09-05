dea6141a0aaa769fbf79f158fe742b422d76423c
Daniel <gleek.dan@gmail.com>
2021-09-04 21:01:23 -0600

    Update auth page styles

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -6,23 +6,30 @@ import { useHistory } from "react-router-dom";
 const AuthPage = () => {
   let history = useHistory();
   return (
-    <div className="container">
-      <h1>Todo App</h1>
-      <h2>Welcome to my todo</h2>
-      <p>What's going to happen tomorrow?</p>
-      <div className="button-wrapper">
-        <Button text="Log In" onClick={() => history.push("/login")} />
-        <Button
-          text="Sign Up"
-          styles={{
-            backgroundColor: "transparent",
-            boxShadow: "none",
-            color: "white",
-          }}
-          onClick={() => history.push("/sign-up")}
-        />
+    <div className="two-row-container">
+      <div className="top-content">
+        <h1>TODO APP</h1>
+        <h2>Welcome to my todo</h2>
+        <p>What's going to happen tomorrow?</p>
       </div>
-      <img className="wave-img" src={wave} alt="wave" />
+      {/* <div className="bottom-content" style={{ background: "green" }}> */}
+      <div
+        className="bottom-content"
+        style={{ backgroundImage: `url(${wave})` }}
+      >
+        <div className="button-wrapper">
+          <Button text="Log In" onClick={() => history.push("/login")} />
+          <Button
+            text="Sign Up"
+            styles={{
+              backgroundColor: "transparent",
+              boxShadow: "none",
+              color: "white",
+            }}
+            onClick={() => history.push("/sign-up")}
+          />
+        </div>
+      </div>
     </div>
   );
 };
