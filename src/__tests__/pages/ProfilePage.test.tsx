e622e4a5cb6fe399e89e29281343717897b5c566
Daniel <gleek.dan@gmail.com>
2021-10-27 22:28:11 -0600

    Add simple profile test

--- src/__tests__/pages/ProfilePage.test.tsx
+++ src/__tests__/pages/ProfilePage.test.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import { render, screen, handlers } from "../../utils/test-utils";
 import { setupServer } from "msw/node";
 import App from "../../components/App";
+import ProfilePage from "../../pages/ProfilePage";
 
+describe("Profile", () => {
+  test("Correct Heading", async () => {
+    render(<ProfilePage />, { route: "/profile" });
+    const logoutBtn = screen.getByRole("button", { name: "Logout" });
+    expect(logoutBtn).toBeInTheDocument();
+  });
+});
+
+// TODO: Look into how to test a logged in user
 describe.skip("Profile Page Renders Correct", () => {
   const server = setupServer(...handlers);
 
